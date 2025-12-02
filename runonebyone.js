567cca7f8cb40de16f8f2e7f607c8da46dc36a1f
Bin Yu <bin.yu.private@gmail.com>
2025-12-02 08:47:58 -0500

    updates

--- runonebyone.js
+++ runonebyone.js
@@ -107,7 +107,7 @@ async function runAllScripts() {
     console.log(`📁 Backed up allskills.md to: ${backupPath.split('/').pop()}\n`);
 
     for (let i = 0; i < topics.length; i++) {
-      if ( iteration == 0 && i < 3) continue;
+      if ( iteration == 0 && i < 28) continue;
       const topic = topics[i];
       console.log(`\n[${i + 1}/${topics.length}] Processing Topic ${topic.code}: ${topic.name}`);
       console.log(`${"─".repeat(50)}`);
